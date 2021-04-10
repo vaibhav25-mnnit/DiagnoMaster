841c54c4b962718326c5e69198b4353af4783062
vaibhav25-mnnit <bagatevaibhav555@gmail.com>
2021-04-10 11:37:39 +0530

    added api and changed fetched data form api
    Co-authored-by: Saurabh gupta <saurabh-38@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const fetch = require('node-fetch')
 require('dotenv').config()
 
 const app = express();
@@ -10,6 +11,18 @@ const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/symptomes', async (req, res) => {
+
+
+    const fetch_URL = 'https://healthservice.priaid.ch/symptoms?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InZiOTgyMjQ0NTkzN0BnbWFpbC5jb20iLCJyb2xlIjoiVXNlciIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3NpZCI6IjYyNjEiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIxMDkiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiMTAwIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiQmFzaWMiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xhbmd1YWdlIjoiZW4tZ2IiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL2V4cGlyYXRpb24iOiIyMDk5LTEyLTMxIiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwc3RhcnQiOiIyMDIxLTA0LTA5IiwiaXNzIjoiaHR0cHM6Ly9hdXRoc2VydmljZS5wcmlhaWQuY2giLCJhdWQiOiJodHRwczovL2hlYWx0aHNlcnZpY2UucHJpYWlkLmNoIiwiZXhwIjoxNjE4MDMwODYwLCJuYmYiOjE2MTgwMjM2NjB9.oRKA5rGv_uaH6aYSn-M_PlhO2I7rLTYRK__yeyIp9kM&format=json&language=en-gb'
+    const response = await fetch(fetch_URL);
+    const data = await response.json();
+
+    console.log(data);
+    res.json(data);
+
 })
 
 app.listen(PORT, () => {
